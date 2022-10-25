1c58ee91c5d7867ec804d08617cbff4ea33311b0
celirojas <mcelinarojas@gmail.com>
2022-10-25 10:02:45 -0300

    Add card text

--- front/pi-grupo8/src/components/RecommendationCard.js
+++ front/pi-grupo8/src/components/RecommendationCard.js
@@ -9,14 +9,29 @@ function RecommendationCard(props) {
       <div className={styles.recommendationImg}>
         <img src={picture} alt="hotel room" />
       </div>
-      <div className={styles.recommendationText}>
-        <div className={styles.recommendationName}>
-          <span>Hotel</span>
-          <h2>Hermitage Hotel</h2>
+      <div className={styles.recommendationInfo}>
+        <div className={styles.recommendationText}>
+          <div className={styles.recommendationName}>
+            <span>Hotel</span>
+            <h2>Hermitage Hotel</h2>
+          </div>
+          <div className={styles.recommendationRating}>
+            <span>8</span>
+            <p>Muy bueno</p>
+          </div>
         </div>
-        <div className={styles.recommendationRating}>
-          <span>8</span>
-          <p>Muy bueno</p>
+        <div className={styles.recommendationDescription}>
+          <p>
+            A 940 del centro <span>MOSTRAR EN EL MAPA</span>
+          </p>
+          <p className={styles.recommendationComment}>
+            American shorthair tabby, for donskoy or american shorthair for
+            malkin. Savannah tiger. Devonshire rex ocicat egyptian mau cornish
+            rex so kitty. <a href="#">Más...</a>
+          </p>
+        </div>
+        <div className={styles.recommendationButton}>
+          <button>Ver más</button>
         </div>
       </div>
     </div>
