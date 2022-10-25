b7e4b6b59559cc757828ab2fe846b8a6b732898c
Melina Baredes <melibaredes@gmail.com>
2022-10-25 20:42:55 -0300

    Primer commit del login, falta responsive

--- front/pi-grupo8/src/App.js
+++ front/pi-grupo8/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import FormLogin from "./components/FormLogin";
+//import Search from "./components/Search";
+import "./Styles/body.module.scss";
+import Categories from "./components/Categories";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <FormLogin/>
+      {/*<Categories/>*/}
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ front/pi-grupo8/src/components/FormLogin.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "../Styles/login.css"
+import App from '../App';
+// import MaterialIcon from 'material-icons-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
+
+
+export default function FormLogin(){
+    const [showPassword, setShowPassword] = React.useState(false)
+    function toggleShowPassword(){
+        setShowPassword(!showPassword)
+    }
+    return(
+        <>
+            <div id="container">
+                <h2>Iniciar Sesión</h2>
+                <form>
+                    <label>Correo electrónico</label>
+                    <input type="text"></input>
+                    <label>Contraseña</label>
+                    <input type={showPassword ? 'text' : 'password'}></input>
+                    {/*<i onClick={()=> setShowPassword(!showPassword)}><MaterialIcon icon={showPassword ? 'visibility_off' : 'visibility'}/></i>*/}
+                    {/* <i onClick={toggleShowPassword}>{showPassword ? <><MaterialIcon icon='visibility_off'/></> : <span><MaterialIcon icon='visibility'/></span> }</i> */}
+                    <i onClick={toggleShowPassword}>{showPassword ? <FontAwesomeIcon icon={faEyeSlash} class="icon" size="lg" style={{color: "#607D8B"}}/> : <FontAwesomeIcon icon={faEye} class="icon" size="lg" style={{color: "#607D8B"}}/> }</i>
+
+                    <button type='submit'>Ingresar</button>
+                    <p>Aún no tienes cuenta? <a href='./FormRegistro'>Regístrate</a></p>
+                </form>
+            </div>
+        </>
+
+    )
+}
