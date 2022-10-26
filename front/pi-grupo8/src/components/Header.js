c50a821594b09eee6b13f030f06973284d656003
celirojas <mcelinarojas@gmail.com>
2022-10-26 16:02:06 -0300

    Add menu to mobile

--- front/pi-grupo8/src/components/Header.js
+++ front/pi-grupo8/src/components/Header.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import styles from '../Styles/header.module.scss'
-import logo from '../assets/logo 1.svg'
+import React from "react";
+import styles from "../Styles/header.module.scss";
+import logo from "../assets/logo 1.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 export default function Header() {
-    return(
-        <header className={styles.header}>
-            <div className={styles.container} >
-                <a className={styles.link} href='#home'><img src={logo} alt="Logo"/></a>
-                <p><a className={styles.link} href='#home'>Sentite como en tu hogar</a></p>
-            </div>
-            <div className={styles.container}>
-                <button className={styles.logginButton}>Crear cuenta</button>
-                <button className={styles.logginButton}>Iniciar sesión</button>
-            </div>
-        </header>
-    )
-    
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <a className={styles.link} href="#home">
+          <img src={logo} alt="Logo" />
+        </a>
+        <p>
+          <a className={styles.link} href="#home">
+            Sentite como en tu hogar
+          </a>
+        </p>
+      </div>
+      <div className={styles.menu}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      <div className={styles.container}>
+        <button className={styles.logginButton}>Crear cuenta</button>
+        <button className={styles.logginButton}>Iniciar sesión</button>
+      </div>
+    </header>
+  );
 }
