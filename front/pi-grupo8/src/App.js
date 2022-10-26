6f2668e4a662ca4562067008866e1a6f47a1d6ee
celirojas <mcelinarojas@gmail.com>
2022-10-26 15:05:52 -0300

    Fix header and footer

--- front/pi-grupo8/src/App.js
+++ front/pi-grupo8/src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 //import Search from "./components/Search";
 import "./Styles/body.module.scss";
 import Categories from "./components/Categories";
 import RecommendationList from "./components/RecommendationList";
+import styles from "./Styles/app.module.scss";
 
 function App() {
   return (
-    <div>
+    <div className={styles.appContainer}>
       <Header></Header>
       <Categories />
       <RecommendationList />
-      {/*<Footer></Footer>*/}
+      <Footer></Footer>
     </div>
   );
 }
