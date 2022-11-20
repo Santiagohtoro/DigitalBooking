44b9b9865b18c6e50412bf44ae84ec37782b03c2
Melina Baredes <melibaredes@gmail.com>
2022-11-20 20:35:44 -0300

    Subiendo jwt test

--- src/main/java/com/example/demo/Model/AppUser.java
+++ src/main/java/com/example/demo/Model/AppUser.java
@@ -1,35 +1,49 @@
 package com.example.demo.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-public class AppUser implements UserDetails {
+@Table(name = "user")
+public class User implements UserDetails {
     @Id
     @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
     private Long id;
     private String nombre;
-    private String username;
+    private String apellido;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRoles appUserRole;
+    @ManyToOne
+    @JoinColumn(name = "ciudad_id")
+    private Ciudad ciudad;
+    @OneToMany( cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonIgnore
+    private Set<Reserva> reservas = new HashSet<>();
 
-    public AppUser() {
+    public User(@NotBlank String name, @NotBlank String surname, @Email String email, String encode, AppUserRoles user, @NotBlank Ciudad city) {
     }
 
-    public AppUser(String nombre, String username, String email, String password, AppUserRoles appUserRole) {
+    public User(Long id, String nombre, String apellido, String email, String password, AppUserRoles appUserRole, Ciudad ciudad) {
+        this.id = id;
         this.nombre = nombre;
-        this.username = username;
+        this.apellido = apellido;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
+        this.ciudad = ciudad;
     }
 
     public Long getId() {
@@ -44,9 +58,6 @@ public class AppUser implements UserDetails {
         this.nombre = nombre;
     }
 
-    public String getUsername() {
-        return username;
-    }
 
     @Override
     public boolean isAccountNonExpired() {
@@ -68,9 +79,6 @@ public class AppUser implements UserDetails {
         return true;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getEmail() {
         return email;
@@ -90,6 +98,11 @@ public class AppUser implements UserDetails {
         return password;
     }
 
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
@@ -100,6 +113,23 @@ public class AppUser implements UserDetails {
 
     public void setAppUserRole(AppUserRoles appUserRole) {
         this.appUserRole = appUserRole;
+    }
+
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
     }
 }
 
