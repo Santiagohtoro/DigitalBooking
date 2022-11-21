b9f47aac4d0139067334025f0c2beda4c4725837
celirojas <mcelinarojas@gmail.com>
2022-11-21 17:16:34 -0300

    Add cities from api

---
+++ front/pi-grupo8/src/api-cities/useApiCities.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+
+const baseUrl = "http://localhost:8081";
+
+const useApiCities = () => {
+  const [data, setData] = React.useState([]);
+
+  const getData = async () => {
+    const response = await axios.get(`${baseUrl}/ciudades/todos`);
+    setData(response.data);
+  };
+  return { data, getData };
+};
+
+export default useApiCities;
