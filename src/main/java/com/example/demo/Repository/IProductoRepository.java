bbd3043a1dd9514c22ada2e53ec71346d4929e9d
Melina Baredes <melibaredes@gmail.com>
2022-12-09 15:18:02 -0300

    Actualización query fecha y ciudad

--- src/main/java/com/example/demo/Repository/IProductoRepository.java
+++ src/main/java/com/example/demo/Repository/IProductoRepository.java
@@ -20,6 +20,6 @@ public interface IProductoRepository extends JpaRepository<Producto, Long> {
     @Query(value = "SELECT producto.* FROM categoria INNER JOIN producto ON producto.categoria_id=categoria.id WHERE categoria.titulo= ?1", nativeQuery = true)
     List<Producto> findByCategoria(String categoria);
 
-    @Query(value = "SELECT p FROM producto p WHERE p NOT IN(SELECT r.producto FROM reserva r WHERE ?2 BETWEEN r.fecha_inicial AND r.fecha_fin OR ?3 BETWEEN r.fecha_inicial AND r.fecha_fin)" + "AND p.ciudad IN (SELECT c FROM ciudad c WHERE c.ciudad = ?1)", nativeQuery = true)
+    @Query(value = "SELECT * FROM producto p WHERE p.id NOT IN(SELECT r.producto_id FROM reserva r WHERE ?2 BETWEEN r.fecha_inicial AND r.fecha_fin OR ?3 BETWEEN r.fecha_inicial AND r.fecha_fin)" + "AND p.ciudad_id IN (SELECT c.id FROM ciudad c WHERE c.ciudad = ?1)", nativeQuery = true)
     List<Producto> findByCiudadAndFecha(String city, LocalDate fechaInicio, LocalDate fechaFin);
 }
