9fbeb81d4d736261e75d01e74aada421aa3dcb33
Santiago Toro <santiagohtoro@outlook.com>
2022-12-10 00:56:03 -0500

    galeria

--- front/pi-grupo8/src/components/Gallery.js
+++ front/pi-grupo8/src/components/Gallery.js
@@ -1,33 +1,52 @@
 import React from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 import galleryStyle from "../Styles/gallery.module.scss";
-import Carousel from 'react-gallery-carousel';
 import 'react-gallery-carousel/dist/index.css';
 
 
 export default function Gallery({ imgs }) {
-  function SeenGallery(imgs) {
-    imgs?.map((img)=>{
-      console.log(img?.url);
-      return <div><Carousel images={img?.url} isMaximized/></div>
-    })
-  }
- 
+  const [open, setOpen] = React.useState(false);
 
   const images = imgs?.map((img, i) =>
-    i < 5 ? (
-      <div
-        key={img?.id}
-        className={galleryStyle.item}
-      >
-        <img src={img?.url} alt={img?.titulo} className={galleryStyle.image} />
-      </div>
-    ) : null
-  );
+  i < 5 ? (
+    <div
+      key={img?.id}
+      className={galleryStyle.item}
+    >
+      <img src={img?.url} alt={img?.titulo} className={galleryStyle.image} />
+    </div>
+  ) : null
+);
 
   return (
     <>
-      <div className={galleryStyle.container}>{images}</div>
-      <button onClick={SeenGallery(imgs)}>Ver Mas</button>
+      <div className={galleryStyle.container}>{images}
+        <button type="button" className={galleryStyle.buttonGallery} onClick={() => setOpen(true)}>
+          Ver Mas
+        </button>
+      </div>
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={[
+          {
+            src: "https://digitalbooking-imagenes-grupo8.s3.us-east-2.amazonaws.com/imgs/categories/apartment-room.jpg",
+            alt: "image 1",
+            width: 3840,
+            height: 2560,
+            srcSet: [
+              { src: "https://digitalbooking-imagenes-grupo8.s3.us-east-2.amazonaws.com/imgs/categories/apartment-room.jpg", width: 320, height: 213 },
+              { src: "https://digitalbooking-imagenes-grupo8.s3.us-east-2.amazonaws.com/imgs/categories/apartment-room.jpg", width: 640, height: 427 },
+              { src: "https://digitalbooking-imagenes-grupo8.s3.us-east-2.amazonaws.com/imgs/categories/apartment-room.jpg", width: 1200, height: 800 },
+              { src: "https://digitalbooking-imagenes-grupo8.s3.us-east-2.amazonaws.com/imgs/categories/apartment-room.jpg", width: 2048, height: 1365 },
+              { src: "https://digitalbooking-imagenes-grupo8.s3.us-east-2.amazonaws.com/imgs/categories/apartment-room.jpg", width: 3840, height: 2560 },
+            ]
+          },
+          // ...
+        ]}
+      />
+      
     </>
   );
 }
---
+++ front/pi-grupo8/src/components/GalleryOnClick.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import Carousel from 'react-responsive-carousel';
+
+
+export default function GalleryOnClick(params) {
+    return(
+        <Carousel>
+            
+        </Carousel>
+    )
+}
