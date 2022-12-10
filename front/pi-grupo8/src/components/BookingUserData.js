7f94b19673d9891b2e5512364bf250cbc124197a
celirojas <mcelinarojas@gmail.com>
2022-12-10 18:29:46 -0300

    fetch user data from context

--- front/pi-grupo8/src/components/BookingUserData.js
+++ front/pi-grupo8/src/components/BookingUserData.js
@@ -1,24 +1,27 @@
 import React from "react";
 import styles from "../Styles/bookingUserData.module.scss";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 function BookingUserData() {
+  const { user } = useAuthContext();
+
   return (
     <div>
       <form className={styles.container}>
         <div className={styles.inputs}>
           <label>
             Nombre
-            <input type="text" placeholder="Juan" />
+            <input type="text" placeholder={user?.name} />
           </label>
           <label>
             Apellido
-            <input type="text" placeholder="Pérez" />
+            <input type="text" placeholder={user?.surname} />
           </label>
         </div>
         <div className={styles.inputs}>
           <label>
             Correo electrónico
-            <input type="email" placeholder="juanperez@gmail.com" />
+            <input type="email" placeholder={user?.username} />
           </label>
           <label>
             Ciudad
