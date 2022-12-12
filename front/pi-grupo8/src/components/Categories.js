c7cb09bc5fcb558827505df8f4e2dc9b0fa23cf1
Santiago Toro <santiagohtoro@outlook.com>
2022-12-12 18:48:41 -0500

    cors

--- front/pi-grupo8/src/components/Categories.js
+++ front/pi-grupo8/src/components/Categories.js
@@ -1,12 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CategoryCard from "./CategoryCard";
 import styles from "../Styles/categories.module.scss";
 import { Outlet } from "react-router-dom";
 import useCategoriesApi from "../api/useCategoriesApi";
+import axios from "axios";
 
 function Categories() {
   const { data, getData } = useCategoriesApi();
-
+  const [categoria, setCategoria] = useState();
+  let config = {
+    method: 'get',
+    url: `http://localhost:8081/productos/categoria/${categoria}`,
+    headers: { }
+  };
+  
+  axios(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
   useEffect(() => {
     getData();
     // eslint-disable-next-line
