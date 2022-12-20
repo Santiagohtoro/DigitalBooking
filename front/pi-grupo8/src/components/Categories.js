7cc70c5dd30c7ecdb598ce257001c93b2e255342
celirojas <mcelinarojas@gmail.com>
2022-12-20 15:41:58 -0300

    peticion reservas

--- front/pi-grupo8/src/components/Categories.js
+++ front/pi-grupo8/src/components/Categories.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 
 import styles from "../Styles/categories.module.scss";
 import { Outlet } from "react-router-dom";
@@ -8,50 +8,38 @@ import stylescardscat from "../Styles/categoryCard.module.scss";
 
 function Categories(props) {
   const { data, getData } = useCategoriesApi();
-  const {setFilterBy} =props;
-  console.log(data)
+  const { setFilterBy } = props;
+  console.log(data);
   // eslint-disable-next-line
-  
-  
-  /*let config = {
-    method: 'get',
-    url: `http://localhost:8081/productos/categoria/${categoria}`,
-    headers: { }
-  };
-  
-  axios(config)
-  .then((response) => {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch((error) => {
-    console.log(error);
-  });*/
+
   useEffect(() => {
     getData();
     // eslint-disable-next-line
   }, []);
 
-  const onChange = (event)=>{
+  const onChange = (event) => {
     const c = event.target.alt;
     setFilterBy(c);
-  }
+  };
 
   return (
     <>
       <div className={styles.categoriesBlock}>
         <h2>Buscar por tipo de alojamiento</h2>
         <div className={styles.categoriesContainer}>
-            {data.map((c, index)=>{
-              return(<div className={stylescardscat.categoryCard} onClick={onChange} >
-                      <div className={stylescardscat.categoryCardImg} >
-                        <img src={c?.imagen?.url} alt={c.titulo} />
-                      </div>
-                      <div className={stylescardscat.categoryCardText}>
-                        <h3>{c.titulo}</h3>
-                        <p>715 {c.titulo}s</p>
-                      </div>
-                    </div>)
-            })}
+          {data.map((c, index) => {
+            return (
+              <div className={stylescardscat.categoryCard} onClick={onChange}>
+                <div className={stylescardscat.categoryCardImg}>
+                  <img src={c?.imagen?.url} alt={c.titulo} />
+                </div>
+                <div className={stylescardscat.categoryCardText}>
+                  <h3>{c.titulo}</h3>
+                  <p>715 {c.titulo}s</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
       <Outlet />
--- front/pi-grupo8/src/components/MyBooking/MyBooking.js
+++ front/pi-grupo8/src/components/MyBooking/MyBooking.js
@@ -1,4 +1,4 @@
-import React , {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import stylesBooking from "../../Styles/myBookingCard.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
@@ -7,65 +7,87 @@ import { faWifi } from "@fortawesome/free-solid-svg-icons";
 import { faSwimmer } from "@fortawesome/free-solid-svg-icons";
 import { useParams } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import axios from "axios";
 
 function MyBooking(props) {
-    useEffect(()=>{
-        
-        // eslint-disable-next-line
-    },[])
-    let { userId } = useParams();
-    console.log(userId)
-    const { user } = useAuthContext();
-    console.log(user);
-    
+  let { userId } = useParams();
+  const { user } = useAuthContext();
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get(
+      `http://ec2-18-217-236-88.us-east-2.compute.amazonaws.com:8081/reservas/todos`,
+      {
+        headers: {
+          Authorization: "Bearer " + user?.token,
+        },
+      }
+    );
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line
+  }, [user]);
+
+  console.log("DATA", data);
+
+  function bookings(data) {
+    data?.map((b) => {
+      return b?.user?.id == userId ? (
+        <div className={stylesBooking.recommendationCard}>
+          <div className={stylesBooking.recommendationImg}>
+            <img src={b?.imagenes?.url} alt={b?.imagenes?.titulo} />
+          </div>
+          <div className={stylesBooking.recommendationInfo}>
+            <div className={stylesBooking.recommendationText}>
+              <div className={stylesBooking.recommendationName}>
+                <span>category</span>
+                <span className={stylesBooking.stars}>
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                </span>
+                <h2>title</h2>
+              </div>
+              <div className={stylesBooking.recommendationRating}>
+                <span>8</span>
+                <p>Muy bueno</p>
+              </div>
+            </div>
+            <div className={stylesBooking.recommendationDescription}>
+              <p className={stylesBooking.recommendationLocation}>
+                <FontAwesomeIcon icon={faLocationDot} />
+                <p>location</p>
+                <span className={stylesBooking.recommendationMap}>
+                  MOSTRAR EN EL MAPA
+                </span>
+              </p>
+              <span className={stylesBooking.recommendationAmmenities}>
+                <FontAwesomeIcon icon={faWifi} />
+                <FontAwesomeIcon icon={faSwimmer} />
+              </span>
+              <p className={stylesBooking.recommendationComment}>
+                description
+                <a href="#url">Más...</a>
+              </p>
+            </div>
+            <div className={stylesBooking.recommendationButton}>
+              <button></button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        console.log("no hay datos")
+      );
+    });
+  }
 
   return (
-        <div className={stylesBooking.recommendationsBlock}>
-            <div className={stylesBooking.recommendationCard}>
-            <div className={stylesBooking.recommendationImg}>
-                <img src alt="hotel room" />
-            </div>
-            <div className={stylesBooking.recommendationInfo}>
-                <div className={stylesBooking.recommendationText}>
-                <div className={stylesBooking.recommendationName}>
-                    <span>category</span>
-                    <span className={stylesBooking.stars}>
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    </span>
-                    <h2>title</h2>
-                </div>
-                <div className={stylesBooking.recommendationRating}>
-                    <span>8</span>
-                    <p>Muy bueno</p>
-                </div>
-                </div>
-                <div className={stylesBooking.recommendationDescription}>
-                <p className={stylesBooking.recommendationLocation}>
-                    <FontAwesomeIcon icon={faLocationDot} />
-                    <p>location</p>
-                    <span className={stylesBooking.recommendationMap}>MOSTRAR EN EL MAPA</span>
-                </p>
-                <span className={stylesBooking.recommendationAmmenities}>
-                    <FontAwesomeIcon icon={faWifi} />
-                    <FontAwesomeIcon icon={faSwimmer} />
-                </span>
-                <p className={stylesBooking.recommendationComment}>
-                    description
-                    <a href="#url">Más...</a>
-                </p>
-                </div>
-                <div className={stylesBooking.recommendationButton}>
-                <button>
-                    
-                </button>
-                </div>
-            </div>
-            </div>
-        </div>
+    <div className={stylesBooking.recommendationsBlock}>{bookings(data)}</div>
   );
 }
 
